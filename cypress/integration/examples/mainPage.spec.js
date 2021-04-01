9d316f510617f1b91329c21aa4688f9e1c4f749e
Sharad Chhunju <chhunjus@gmail.com>
2021-04-01 15:39:08 +0545

    new tests

--- cypress/integration/examples/mainPage.spec.js
+++ cypress/integration/examples/mainPage.spec.js
@@ -3,16 +3,55 @@
 describe('Actions', () => {
     beforeEach('Logins',() => {
       cy.login()
+
+      cy.intercept('GET','http://localhost:8081/api/passwords/get-passwords?user-id=2').as('password')
     })
     
     it('Visits login page', () => {
+      
         cy.visit('/dashboard')
         cy.get('.toaster-title').should('be.visible').should('have.text','Login Success')
       cy.get('.user-name > .sub-title').should('have.text','Nischal Babu Bohara')
       cy.get('.logo').should('be.visible')
+    })
+
+    it('get user id', () => {
+
+        cy.visit('/dashboard')
       
+        cy.wait('@password')
+            .should(({ request, response }) => {
+                // expect(request.url).to.match('/\/user$/')        // regex aayena
+                expect(request.method).to.equal('GET')
+                expect(response.body).to.have.property('passwordList')
+                // expect(response.body.passwordList).to.have.lengthOf(3)
+                cy.log(response.body.passwordList)
+        })
+    
     })
   
+    it.only('get user id', () => {
+      cy.visit('/')
+      cy.fixture('pwd').then((pwd)=>{
+        cy.request({method:'POST', 'auth':{
+          'bearer' : window.localStorage.getItem('token')
+        },
+        url:'http://localhost:8081/api/passwords/save-password', failOnStatusCode: false, body: pwd})
+      })
+      cy.reload()
+      cy.wait('@password').then((resp)=>{
+        expect(resp.response.body).to.have.property('passwordList');
+        const {
+          passwordList
+        } = resp.response.body.passwordList.length;
+        const pwdLength = resp.response.body.passwordList.length
+        cy.log(resp.response.body.passwordList.length)
+        if(pwdLength != 0){
+          cy.get(`.content-table > tbody > :nth-child(${pwdLength}) > :nth-child(3)`).should('not.be.empty')
+        }
+      })
+    })
+
   
 })
   
