a74b38180f0a9587790ada22dcd2845692d0aca6
Sharad Chhunju <chhunjus@gmail.com>
2021-06-10 12:15:08 +0545

    added integration and system tests

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,104 @@
+/// <reference types="cypress" />
+
+
+describe('Login Using Email', () =>{
+    const wrongPassword = 'qqqqqq'
+    let email, password, firstName;
+    before(()=>{
+        cy.fixture('user').then((data)=>{
+            email = data.email
+            password = data.password
+            firstName = data.firstName
+        })
+    })
+
+    beforeEach('Visits Login Page',() => {
+        cy.visit('/');   
+    })
+
+    it('can fill the form', ()=>{
+        cy.get('input[type="email"][name="email"]').type(email)
+            .should('have.value',email).clear()
+            .should('have.value','');
+        cy.get('input[type="password"][name="password"]').type(password)
+        cy.get('button[type="submit"]')
+            .contains('Login')
+    })
+
+    it('requires both email and password',()=>{
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-text').eq(0).should('have.text','Email cannot be empty. Invalid Email.');
+        cy.get('.error-text').eq(1).should('have.text','Password cannot be empty.Password should be more than 8 characters.')
+    })
+
+    it('requires email',()=>{
+        cy.get('input[type="password"][name="password"]').type(password)
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-text').should('have.text','Email cannot be empty. Invalid Email.');
+    })
+
+    it('requires password',()=>{
+        cy.get('input[type="email"][name="email"]').type(email)
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-text').should('have.text','Password cannot be empty.Password should be more than 8 characters.')
+    })
+
+    it('requires password greater than 8',()=>{
+        cy.get('input[type="email"][name="email"]').type(email)
+        cy.get('input[type="password"][name="password"]').type('qqqq')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-text').should('have.text','Password should be more than 8 characters.')
+    })
+
+    it.skip('shows email or password invalid.',()=>{
+        cy.get('input[type="email"][name="email"]').type(email)
+        cy.get('input[type="password"][name="password"]').type(wrongPassword)
+        cy.get('button[type="submit"]').click()
+        cy.get('.error-text').should('have.text','Password should be more than 8 characters.')
+    })
+
+    describe('Email Validation', () =>{
+        const invalidEmail1 = 'asdfghh'
+        const invalidEmail2 = 'asdfghh@'
+        const invalidEmail3 = 'asdfghh@sdfsdf'
+
+        it('requires valid email for email format: ' + invalidEmail1,()=>{
+             //first case email with no @ in email
+            cy.get('input[type="email"][name="email"]').clear().type(invalidEmail1)
+            cy.get('button[type="submit"]').click()
+            cy.get('input[type="email"]').then(($input) => {
+                expect($input[0].validationMessage).to.eq(`Please include an '@' in the email address. '${invalidEmail1}' is missing an '@'.`)
+            })
+            cy.get('.error-text').eq(0).click().should('have.text',' Invalid Email.');
+        })
+
+        it('requires valid email for email format: '+ invalidEmail2,()=>{
+            //second case email with email@ only.
+            cy.reload()
+            cy.get('input[type="email"][name="email"]').clear().type(invalidEmail2)
+            cy.get('button[type="submit"]').click()
+            cy.get('input[type="email"]').then(($input) => {
+                expect($input[0].validationMessage).to.eq(`Please enter a part following '@'. '${invalidEmail2}' is incomplete.`)
+            })
+            cy.get('.error-text').eq(0).click().should('have.text',' Invalid Email.');
+        })
+
+        it('requires valid email for email format: '+ invalidEmail3,()=>{
+            //third case with no '.com'
+            cy.reload()
+            cy.get('input[type="email"][name="email"]').clear().type(invalidEmail3)
+            cy.get('button[type="submit"]').click()
+            cy.get('.error-text').eq(0).should('have.text',' Invalid Email.');
+        })
+    })
+
+    it('navigates to homepage on successful login',()=>{
+        cy.get('input[type="email"][name="email"]').type(email)
+        cy.get('input[type="password"][name="password"]').type(password)
+        cy.intercept('POST','http://localhost:8080/api/users/login').as('login')
+        cy.get('button[type="submit"]').click()
+        cy.wait('@login')
+        cy.get('.user-name > .sub-title').contains(firstName).should('be.visible')
+    })
+
+})
---
+++ cypress/integration/examples/userMainPage.spec.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+describe('Actions', () => {
+  let userId;
+  let userDetail;
+  beforeEach('Logins',() => {
+    cy.fixture('user').then((resp)=>{
+      userDetail = resp;
+      cy.login(resp.email, resp.password).then((data)=>{
+        userId = data.id
+        //always check for user id.
+        cy.intercept('GET',`http://localhost:8080/api/passwords/get-passwords?user-id=${userId}`).as('password')
+      })
+    })
+    cy.visit('/')
+  })
+
+  it('xhr test: Check GET password response.', () => {
+    cy.log(userId)
+    cy.wait('@password')
+        .should(({ request, response }) => {
+            // expect(request.url).to.match('/\/user$/')        // regex aayena
+            expect(request.method).to.equal('GET')
+            expect(response.body).to.have.property('passwordList')
+            // expect(response.body.passwordList).to.have.lengthOf(3)
+            cy.log(response.body.passwordList)
+    })
+  })
+
+  it('Check for display of latest added password ', () => {
+    cy.fixture('pwd').then((pwd)=>{
+      //add new password.
+      cy.request({
+        method:'POST', 
+        'auth':{
+        'bearer' : window.localStorage.getItem('token')
+        },
+        //check for user id in pwd.json
+        url:'http://localhost:8080/api/passwords/save-password', failOnStatusCode: false, body: pwd
+      })
+    })
+    cy.reload()
+    cy.wait('@password').then((resp)=>{
+      expect(resp.response.body).to.have.property('passwordList');
+      const pwdLength = resp.response.body.passwordList.length
+      cy.log(resp.response.body.passwordList.length)
+      if(pwdLength != 0){
+        //the latest added password should not be empty.
+        cy.get(`.content-table > tbody > :nth-child(${pwdLength}) > :nth-child(3) `).should('not.be.empty')
+      }
+    }) 
+  })
+
+  it('decrypt last/bottom password',()=>{
+    //CHECK IF added password is decrypted or not.
+    cy.wait('@password').then((resp)=>{
+      const pwdLength = resp.response.body.passwordList.length
+      //click on decrypt password button icon
+      cy.get(`:nth-child(${pwdLength}) > :nth-child(5) > .actions > .pointer`).eq(1).click()
+      cy.get('input[name="originalPassword"][type="password"]').type(userDetail.password)
+      cy.intercept('POST','http://localhost:8080/api/passwords/decrypt-password').as('getPassword')
+      cy.get('.button-area > .btn').contains('Confirm').click()
+      cy.wait('@getPassword').then((resp)=>{
+        cy.get(`.content-table > tbody > :nth-child(${pwdLength}) > :nth-child(3) `).should('have.text',resp.response.body.decryptedPassword.password)
+      })
+    })
+    
+    
+  })
+  
+})
+  
