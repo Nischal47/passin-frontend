d21f041ae63b0490684b0d4029be05f4b16b7621
Sharad Chhunju <chhunjus@gmail.com>
2021-06-12 10:28:57 +0545

    Random Password Generator Test Added

---
+++ cypress/integration/examples/randomPasswordGenerator.spec.js
@@ -0,0 +1,19 @@
+describe('Actions', () => {
+    beforeEach('Logins',() => {
+      cy.fixture('user').then((resp)=>{
+        cy.login(resp.email, resp.password)
+        cy.visit('/')
+      })
+    })
+
+    it('Generates Random Password',()=>{
+        cy.get('.btn').contains('Add Password').click()
+        cy.intercept('GET','http://localhost:8080/api/passwords/generate-random-password?minLength=8&maxLength=12').as('randomPassword')
+        cy.get('.btn.pointer').contains('Generate Password').click()
+        cy.wait('@randomPassword').then(resp=>{
+            const rPwd = resp.response.body.password
+            expect(rPwd).to.match(/^(?=.*[A-Z])(?=.*[\W])(?=.*[0-9])(?=.*[a-z]).{8}$/)
+            cy.get('input[type="password"][name="password"]').should('have.value',rPwd)
+        })
+    })
+})  
